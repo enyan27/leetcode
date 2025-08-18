ba1bcb92c77168f7b5b9908e33724f1d965b800d
えにゃん <truongdat2325@gmail.com>
2025-08-18 13:45:42 +0700

    Time: 0 ms (100.00%) | Memory: 56.1 MB (93.49%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    if (!nums || nums.length === 0) return 0;
+    let k = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[k] !== nums[i]) {
+            k++;
+            nums[k] = nums[i];
+        }
+    }
+
+    return k + 1;
+};
