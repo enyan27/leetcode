fd230813b6c86f6ee009e12304d0bc04a93f300f
えにゃん <truongdat2325@gmail.com>
2025-09-24 10:17:41 +0700

    Time: 3 ms (56.88%) | Memory: 61.4 MB (66.31%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    let dummyHead = new ListNode(0);
+    let current = dummyHead;
+    let carry = 0;
+
+    while (l1 !== null || l2 !== null || carry !== 0) {
+        let val1 = l1 ? l1.val : 0;
+        let val2 = l2 ? l2.val : 0;
+
+        let sum = val1 + val2 + carry;
+        carry = Math.floor(sum / 10); // nếu sum >= 10 thì carry = 1
+        let digit = sum % 10; // chỉ lấy chữ số hàng 'đơn vị' để ghi vào node mới
+
+        current.next = new ListNode(digit);
+        current = current.next;
+
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+
+    return dummyHead.next;
+};
